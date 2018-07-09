55747342f3018fe7ab739ca2543aa66812c33fae
LeeHanYeong <dev@lhy.kr>
2018-07-09 16:38:00 +0900

    RDS설정

--- README.md
+++ README.md
@@ -17,6 +17,25 @@
 }
 ```
 
+#### `.secrets/dev.json`
+
+- PostgreSQL을 사용, DATABASES섹션의 설정이 필요
+
+```json
+{
+  "DATABASES": {
+    "default": {
+      "ENGINE": "django.db.backends.postgresql",
+      "HOST": "<host>",
+      "PORT": 5432,
+      "USER": "<user>",
+      "PASSWORD": "<password>",
+      "NAME": "<db name>"
+    }
+  }
+}
+```
+
 ## Installation
 
 ```
---
+++ app/config/settings/dev.py
@@ -0,0 +1,17 @@
+from .base import *
+# secrets = json.loads(open(os.path.join(SECRETS_DIR, 'dev.json')).read())
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'dev.json')))
+
+DEBUG = True
+
+# Static
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+
+# WSGI
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# DB
+DATABASES = secrets['DATABASES']
